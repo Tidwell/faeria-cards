f10a6a1268ff23b4b00b076a7af0f35101dd5f77
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2016-02-26 14:25:55 -0500

    update

--- runner.sh
+++ runner.sh
@@ -1,4 +1,4 @@
-cp ~/Library/Application\Support/Steam/SteamApps/common/Faeria/Faeria.app/Contents/Resources/Data/resources.assets ./;
+cp ~/Library/Application\ Support/Steam/SteamApps/common/Faeria/Faeria.app/Contents/Resources/Data/resources.assets ./;
 
 java -jar ./disunity_v0.5.0/disunity.jar asset unpack resources.assets;
 
